6104c2794e55aca443699628287ef561b4669330
Kauan Cristian <kauancristian.lz08@gmail.com>
2025-07-29 13:07:15 -0300

    Up Protfolio
    Sessão de contato feita, ajustes de cor, plano de fundo alterado, scrollReveal aplicado

---
+++ app/main/scroll,.js
@@ -0,0 +1,50 @@
+  ScrollReveal().reveal('#aboutMe', {
+    delay: 50,       
+    duration: 1000,     
+    distance: '50px',   
+    origin: 'bottom',   
+    reset: false   
+});
+
+ScrollReveal().reveal("#textAbout", {
+    delay: 150,       
+    duration: 1000,     
+    distance: '50px',   
+    origin: 'bottom',   
+    reset: false 
+});
+
+ScrollReveal().reveal("#boxsAbout", {
+    delay: 300,       
+    duration: 1000,     
+    distance: '50px',   
+    origin: 'bottom',   
+    reset: false 
+});
+
+ScrollReveal().reveal("#skillsBox", {
+    delay: 300,       
+    duration: 1000,     
+    distance: '50px',   
+    origin: 'bottom',   
+    reset: false 
+});
+
+ScrollReveal().reveal("#boxCode", {
+    delay: 300,       
+    duration: 1000,     
+    distance: '50px',   
+    origin: 'bottom',   
+    reset: false 
+});
+
+
+ScrollReveal().reveal("#contactMe", {
+    delay: 400,       
+    duration: 1000,     
+    distance: '50px',   
+    origin: 'bottom',   
+    reset: false 
+});
+
+
